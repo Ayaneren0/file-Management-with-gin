a061d589f7c495c6d8497c7a688c80eb58353dfe
Ayan Ahmad <www.ayan007ahmad@gmail.com>
2024-08-02 12:16:50 +0530

    add func main and initilize routes for handlers

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 //File repersents the structure of a file in the application.
 type File struct {
 	ID        string
 	Name      string
 	Path      string
 	Thumbnail string
+}
+
+//files holds list of uploaded file.
+var files = &File{}
+
+func main() {
+	r := gin.Default()
+	//Serve static files from "static" directory.
+	r.Static("/static", "./static")
+	// Load HTML templates from "templates" directory.
+	r.LoadHTMLGlob("templates/*.html")
+
+	//route to handle
+	r.GET("/", homeHandler)              //home page
+	r.GET("/upload", uploadHandler)      //upload page
+	r.GET("/:id", fileHandler)           //individual file display
+	r.POST("/upload", PostUploadHandler) //file uploads
+
+	// Start the server on port 8080.
+	r.Run(":8080")
 }
